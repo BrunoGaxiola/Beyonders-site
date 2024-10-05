d259095924abc7b2501ea34ecc0fa6af585fa726
BrunoGaxiola <A01253874tec.mx>
2024-10-05 14:30:20 -0700

    feat: Navis and pages 1 and 2

--- src/SecondPage/SecondPage.jsx
+++ src/SecondPage/SecondPage.jsx
@@ -2,18 +2,14 @@ import './SecondPage.css'
 
 const SecondPage = () => {
     return(
-    <>
-        <div className="desc">
-            <p className="dialog">
-            It seems you are very far from your home. “Earth”, you call it? I always see it from afar. But in the meantime, 
-            come with me. I want to show you some really cool stuff, called “Exoplanets”, doesn't that sound cool?
-            </p>
-            <div className="mascot">
-                <p>aaaaaaaaaaaaa</p>
-            </div>
+        <>
+        <div className="secondPageContainer">
+            <p className="dialog">It seems you are very far from your home. “Earth”, you call it? I always see it from afar. 
+                But in the meantime, come with me. I want to show you some really cool stuff, called “Exoplanets”, doesn't that sound cool?</p>
+            <div className="mascot">aaaaaaaaaaaa</div>
         </div>
-        <button className="next">Next</button>
-    </>
+        <button className="next" onClick={() => navigate('/thirdPage')}> Next </button>
+        </>
     );
 }
 
