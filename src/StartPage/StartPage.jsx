5e7c55b956fa9b5a3f7c4fc0a3e25b0e74b1b39f
BrunoGaxiola <A01253874tec.mx>
2024-10-06 10:46:23 -0700

    feat: almost all changes

--- src/StartPage/StartPage.jsx
+++ src/StartPage/StartPage.jsx
@@ -1,22 +1,22 @@
-import { Navigate, useNavigate } from "react-router-dom";
-import "./StartPage.css"
+import { useNavigate } from "react-router-dom";
+import "./StartPage.css";
+import bgVideo from '../assets/intro.mp4';
 
 const StartPage = () => {
-
     const navigate = useNavigate();
-    return(
-        <>
-            <div className="startPageContainer">
-                <div className="background">
-                    <div className="allText">
-                        <p className="title"> Beyonders </p>
-                        <p className="descStartPage"> Hey! Who are you? </p>
-                        <button className="start" onClick={() => navigate('/firstPage')}> Let's Get Started! </button>
-
-                    </div>
+    return (
+        <div className="startPageContainer">
+            <div className="background">
+                <div className="allText">
+                    <p className="title">Beyonders</p>
+                    <p className="descStartPage">Hey! Who are you?</p>
+                    <button className="start" onClick={() => navigate('/firstPage')}>
+                        Let's Get Started!
+                    </button>
                 </div>
+                <video src={bgVideo} autoPlay muted className="backgroundVideo"></video>
             </div>
-        </>
+        </div>
     );
 }
 
